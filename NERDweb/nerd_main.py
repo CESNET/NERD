20e40c1470e294419c827722684f0d7a7f3d0709
Vaclav Bartos <bartos@cesnet.cz>
2016-06-02 16:24:49 +0200

    NERDweb: search form improved; other minor improvements elsewhere

--- NERDweb/nerd_main.py
+++ NERDweb/nerd_main.py
@@ -8,12 +8,13 @@ import pytz
 from flask import Flask, request, render_template, g, jsonify
 from flask.ext.pymongo import PyMongo, ASCENDING, DESCENDING
 from flask_wtf import Form
-from wtforms import validators, TextField, IntegerField, BooleanField
+from wtforms import validators, TextField, IntegerField, BooleanField, SelectField
 
 #import db
 import ctrydata
 
-WARDEN_DROP_PATH = "/data/warden_filer/warden_receiver/incoming"
+#WARDEN_DROP_PATH = "/data/warden_filer/warden_receiver/incoming"
+WARDEN_DROP_PATH = "f:/CESNET/RepShield/NERD/NERDd/warden_filer/incoming"
 
 app = Flask(__name__)
 
@@ -29,21 +30,6 @@ app.jinja_env.lstrip_blocks = True
 
 mongo = PyMongo(app)
 
-def get_int_arg(argname, default=None, min=None, max=None):
-    """Get argument from request and convert it to int. If argument is not 
-    present or it can't be converted to int, return default.
-    If min or max is specified and value is lower/greater than min/max, return
-    value of min/max instead."""
-    try:
-        val = int(request.args.get(argname, default))
-        if min is not None and val < min:
-            return min
-        elif max is not None and val > max:
-            return max
-        return val
-    except ValueError:
-        return default
-
 
 # ***** Main page *****
 @app.route('/')
@@ -53,16 +39,48 @@ def main():
 # ***** List of IP addresses *****
 
 class IPFilterForm(Form):
-    limit = IntegerField('Max number of addresses', [])
+    subnet = TextField('IP prefix', [validators.Optional()])
+    country = TextField('Country code', [validators.Optional(), validators.length(2, 2)])
+    sortby = SelectField('Sort by', choices=[
+                ('events','Events'),
+                ('ts_update','Last update'),
+                ('ts_added','Time added'),
+                ('ip','IP address'),
+             ], default='events')
+    asc = BooleanField('Ascending', default=False)
+    limit = IntegerField('Max number of addresses', [validators.NumberRange(1, 1000)], default=20)
+
+sort_mapping = {
+    'events': 'events.total',
+    'ts_update': 'ts_last_update',
+    'ts_added': 'ts_added',
+    'ip': '_id',
+}
 
 @app.route('/ips')
 @app.route('/ips/')
 def ips():
-    limit = get_int_arg('limit', 20, min=1, max=1000)
-    form = IPFilterForm(limit=limit)
-    ipinfo = mongo.db.ip.find().limit(limit)
-    timezone = pytz.timezone('Europe/Prague') # TODO autodetect (probably better in javascript)
-    #ipinfo = [db.getIPInfo('.'.join(str(random.randint(0,255)) for _ in range(4))) for _ in range(limit)]
+    title = "IP search"
+    form = IPFilterForm(request.args, csrf_enabled=False)
+    if form.validate():
+        print("Validation OK")
+        print(form.asc.data)
+        timezone = pytz.timezone('Europe/Prague') # TODO autodetect (probably better in javascript)
+        sortby = sort_mapping[form.sortby.data]
+        # Prepare 'find' part of the query
+        queries = []
+        if form.subnet.data:
+            subnet = form.subnet.data
+            subnet_end = subnet[:-1] + chr(ord(subnet[-1])+1)
+            queries.append( {'$and': [{'_id': {'$gte': subnet}}, {'_id': {'$lt': subnet_end}}]} )
+        if form.country.data:
+            queries.append( {'geo.ctry': form.country.data.upper() } )
+        query = {'$and': queries} if queries else None
+        # Perform DB query
+        print("Query: "+str(query))
+        ipinfo = mongo.db.ip.find(query).limit(form.limit.data).sort(sortby, 1 if form.asc.data else -1)
+    else:
+        ipinfo = None
     return render_template('ips.html', ctrydata=ctrydata, sorted=sorted, **locals())
 
 
@@ -73,6 +91,7 @@ class AlertFilterForm(Form):
 
 @app.route('/events')
 def events():
+    title = "Events"
     limit = get_int_arg('limit', 10, min=1, max=1000)
     skip = get_int_arg('skip', 0, min=0)
     num_alerts = mongo.db.alerts.count()
@@ -94,10 +113,12 @@ def ip(ipaddr=None):
     form = SingleIPForm(ip=ipaddr)
     #if form.validate():
     if ipaddr:
+        title = ipaddr
         ipinfo = mongo.db.ip.find_one({'_id':form.ip.data})
     else:
+        title = 'IP detail search'
         ipinfo = {}
-    return render_template('ip.html', form=form, ip=form.ip.data, ipinfo=ipinfo, ctrydata=ctrydata)
+    return render_template('ip.html', ctrydata=ctrydata, sorted=sorted, ip=form.ip.data, **locals())
 
 
 # ***** NERD status information *****
