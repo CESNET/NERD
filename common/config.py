8be7a2cf7fba0f863655c380627666d68845f5b2
Vaclav Bartos <bartos@cesnet.cz>
2016-10-18 13:49:32 +0200

    config: added hierarchical update - allows to merge to dicts
    including unittests

--- common/config.py
+++ common/config.py
@@ -32,9 +32,42 @@ def hierarchical_get(self, key, default=NoDefault):
             raise MissingConfigError("Mandatory configuration element is missing: " + key)
         else:
             return default
+
+def hierarchical_update(self, other):
+    """
+    Update HierarchicalDict with other dictionary and merge common keys.
     
-class DictWithHierarchicalGet(dict):
+    If there is a key in both current and the other dictionary and vlaues of
+    both keys are dictionaries, they are merged together.
+    Example:
+      HierarchicalDict({'a': {'b': 1, 'c': 2}}).update({'a': {'b': 10, 'd': 3}})
+      ->
+      HierarchicalDict({'a': {'b': 10, 'c': 2, 'd': 3}})
+    
+    Changes the dictionary directly, returns None.
+    """
+    other = dict(other)
+    for key in other.keys():
+        if key in self:
+            if isinstance(self[key], dict) and isinstance(other[key], dict):
+                # The key is present in both dicts and both key values are dicts -> merge them
+                hierarchical_update(self[key], other[key])
+            else:
+                # One of the key values is not a dict -> overwrite the value 
+                # in self by the one from other (like normal "update" does)
+                self[key] = other[key]
+        else:
+            # key is not present in self -> set it to value from other
+            self[key] = other[key]
+
+
+class HierarchicalDict(dict):
     get = hierarchical_get
+    update = hierarchical_update
+    def __repr__(self):
+        return 'HierarchicalDict({})'.format(dict.__repr__(self))
+    def copy(self):
+        return HierarchicalDict(dict.copy(self))
 
 
 def read_config(file):
@@ -64,4 +97,127 @@ def read_config(file):
         configstr = re.sub(',(?=\s*[}\]])', '', configstr)
         # Load as JSON
         conf_dict = json.loads(configstr)
-    return DictWithHierarchicalGet(conf_dict)
+    return HierarchicalDict(conf_dict)
+
+
+
+# ***** Unit tests *****
+# TODO 'update' is tested only, test 'get' as well
+
+if __name__ == '__main__':
+    import unittest
+    from copy import deepcopy
+    class HierarchicalUpdateTest(unittest.TestCase):
+        def runTest(self):
+            # Testing dicts
+            d1 = HierarchicalDict({
+                'a': 'a',
+                'b': {
+                    'b1': 1,
+                    'b2': [1,2,3],
+                },
+                'c': {
+                    'c1': {
+                        'c1x': 'qwer',
+                        'c1y': 'asdf',
+                    },
+                    'c2': 2,
+                }
+            })
+            d2 = HierarchicalDict({
+                'b': {
+                    'b1': 12345,
+                    'b3': 'BBB',
+                    'b4': {
+                        'b4a': None
+                    },
+                },
+                'c': {
+                    'c2': {
+                        'c20': None
+                    },
+                },
+                'd': 'x',
+                'e': {}
+            })
+            d3 = {
+                'a': 'A',
+            }
+            d4 = [('c', ['test']), ('d', 'y')]
+            d5 = {'c': {'c1': {'c1z': None}}}
+            d_empty = {}
+            
+            # Expected results
+            d1d2 = HierarchicalDict({
+                'a': 'a',
+                'b': {
+                    'b1': 12345,
+                    'b2': [1,2,3],
+                    'b3': 'BBB',
+                    'b4': {
+                        'b4a': None
+                    },
+                },
+                'c': {
+                    'c1': {
+                        'c1x': 'qwer',
+                        'c1y': 'asdf',
+                    },
+                    'c2': {
+                        'c20': None
+                    },
+                },
+                'd': 'x',
+                'e': {}
+            })
+            d2d3d4 = HierarchicalDict({
+                'a': 'A',
+                'b': {
+                    'b1': 12345,
+                    'b3': 'BBB',
+                    'b4': {
+                        'b4a': None
+                    },
+                },
+                'c': ['test'],
+                'd': 'y',
+                'e': {}
+            })
+            d1d5 = HierarchicalDict({
+                'a': 'a',
+                'b': {
+                    'b1': 1,
+                    'b2': [1,2,3],
+                },
+                'c': {
+                    'c1': {
+                        'c1x': 'qwer',
+                        'c1y': 'asdf',
+                        'c1z': None,
+                    },
+                    'c2': 2,
+                }
+            })
+            
+            # Tests
+            res = deepcopy(d1)
+            res.update(d2)
+            self.assertEqual(res, d1d2, "Update of d1 by d2 failed")
+            
+            res = deepcopy(d2)
+            res.update(d3)
+            res.update(d4)
+            self.assertEqual(res, d2d3d4, "Update of d2 by d3 and d4 failed")
+            
+            res = deepcopy(d1)
+            res.update(d5)
+            self.assertEqual(res, d1d5, "Update of d1 by d5 failed")
+            
+            res = deepcopy(d1)
+            res.update(d_empty)
+            res.update([])
+            res.update(HierarchicalDict())
+            self.assertEqual(res, d1, "dict has changed after update by empty dict")
+    
+    unittest.main()
+            
