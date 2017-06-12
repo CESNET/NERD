34553e79aa5df6b0edfee4b562221cd88423426e
Vaclav Bartos <bartos@cesnet.cz>
2017-06-12 15:40:20 +0200

    Scripts: removing of old records from MongoDB made quiet
    So it can be better used in cron

--- scripts/nerd_db_remove_old_records.sh
+++ scripts/nerd_db_remove_old_records.sh
@@ -2,5 +2,7 @@
 # Removes all records last updated more than 14 days ago from NERD database.
 
 d=$(date +%Y-%m-%d -d "14 days ago")
-mongo nerd --eval "db.ip.remove({ts_last_event: {\$lt: ISODate(\"${d}T00:00:00\")}})"
+
+# "var x" supresses output of the function result
+mongo nerd --quiet --eval "var x = db.ip.remove({ts_last_event: {\$lt: ISODate(\"${d}T00:00:00\")}})"
 
