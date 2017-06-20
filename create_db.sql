19536720f7d9348756a274c0ff2abffbe0704c4c
Vaclav Bartos <bartos@cesnet.cz>
2017-06-20 13:47:38 +0200

    API: api_token added to user DB scheme; script to set API token

--- create_db.sql
+++ create_db.sql
@@ -5,8 +5,7 @@ CREATE TABLE IF NOT EXISTS users (
 	name	VARCHAR,
 	email	VARCHAR,
 	org	VARCHAR,
-	api_id	VARCHAR,
-	api_secret VARCHAR
+	api_token	VARCHAR,
 );
 -- testing user (used when testing on localhost by directly running nerd_main.py)
 INSERT INTO users (id,groups,name,email) VALUES ('test_user','{"registered","test"}','Mr. Test','test@example.org') ON CONFLICT DO NOTHING;
---
+++ scripts/set_api_token.sh
@@ -0,0 +1,17 @@
+#!/bin/sh
+# Set API token to given user (in 'users' table in PSQL DB 'nerd').
+# If no token is specified, a random string of 10 alphanumeric characters is used.
+
+if [ -z "$1" ]; then
+  echo -e "Usage:\n   $0 user_name [token]" >&2
+  exit 1
+fi
+user=$1
+if [ -z "$2" ]; then
+  token="$(</dev/urandom tr -dc 'a-zA-Z0-9' | head -c 10)"
+else
+  token="$2"
+fi
+echo "Setting token '$token' to user '$user'"
+psql -U nerd -c "UPDATE users SET api_token = '$token' WHERE id = '$user'"
+
