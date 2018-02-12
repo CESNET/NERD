bddb7daea47a21d8c0d009f0d811eac5bd0f6fbd
Tomas Jansky <janskto1@fit.cvut.cz>
2018-02-12 14:44:43 +0100

    removed unwanted files from pull request

--- NERDweb/wsgi.py
+++ NERDweb/wsgi.py
@@ -1,5 +1,2 @@
 from nerd_main import app as application
 application.debug = True # Enable logging of exceptions into Apache log file
-from nerd_main import config
-config.testing = True
-
--- drop_db.sh
+++
@@ -1,2 +0,0 @@
-/usr/bin/mongo nerd --quiet --eval 'db.ip.drop(); db.asn.drop(); db.bgppref.drop(); db.ipblock.drop(); db.org.drop()'
-/usr/bin/psql -c 'delete from events;' nerd nerd
