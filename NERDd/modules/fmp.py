9fe20203ae685b16df9f1a41127549ab740d238d
Tomas Jansky <janskto1@fit.cvut.cz>
2018-11-22 16:39:02 +0100

    fmp: implemented logging mechanism

--- NERDd/modules/fmp.py
+++ NERDd/modules/fmp.py
@@ -3,15 +3,26 @@ NERD module
 """
 from core.basemodule import NERDModule
 import g
+import threading
 
 import logging
 import numpy as np
 from datetime import datetime, timedelta, timezone
+import re
+import os
+import fcntl
 
 class FMP(NERDModule):
     def __init__(self):
         self.log = logging.getLogger("FMPmodule")
         #self.log.setLevel('DEBUG')
+        self.paths = g.config.get("fmp.paths", {"general" : "/data/fmp/general/"})
+        for key, value in self.paths.items():
+            if not os.path.exists(value):
+                os.makedirs(value)
+                os.makedirs(value + "result/")
+
+        np.set_printoptions(formatter={'float_kind': lambda x: "{:7.4f}".format(x)})
 
         # Register all necessary handlers.
         g.um.register_handler(
@@ -31,12 +42,13 @@ class FMP(NERDModule):
 
         feat_v = np.array([])
         feat_v = np.resize(feat_v, 21)
+        attacked = 0
 
         i = 0;
         if 'events_meta' in rec:
             metadata = rec['events_meta']
             # Alerts 1d
-            feat_v[i] = metadata.get('total1', 0)
+            feat_v[i] = attacked = metadata.get('total1', 0)
             i += 1
             # Alerts 7d
             feat_v[i] = metadata.get('total7', 0)
@@ -58,6 +70,8 @@ class FMP(NERDModule):
 
         # Last alert age
         feat_v[i] = (datetime.utcnow() - rec['ts_last_event']).total_seconds() / 86400
+        if feat_v[i] > 7.0:
+            feat_v[i] = inf
         i += 1
 
         # Blacklists
@@ -104,10 +118,29 @@ class FMP(NERDModule):
         else:
             i += 4
 
-        
+        # TODO: load data model, process feature vector
+        #actions.append(('set', 'fmp.general', '...'))
 
-        self.log.debug('FV: {}'.format(feat_v))
+        self.logFMP(ip, feat_v, 0.5, attacked, self.paths['general'])
 
-
-        #actions.append(('set', 'fmp.general', '0.5'))
         return actions
+
+    def logFMP(self, ip, fv, fmp, attacked, path):
+        curTime = datetime.utcnow()
+        logTime = curTime.strftime("%Y-%m-%dT%H:%M:%S")
+        fileSuffix = curTime.strftime("%Y_%m_%d")
+        attacked_bin = '1' if attacked > 0 else '0'
+        prefix = logTime + ',' + ip + ','
+        suffix = ",{:.4f}".format(fmp)
+
+        f = open(path + fileSuffix, 'a')
+        fcntl.flock(f, fcntl.LOCK_EX)
+        f.write(prefix + re.sub(r"[ \]\[]", r"", np.array2string(fv, max_line_width=1000, separator=',')) + suffix + '\n')
+        fcntl.flock(f, fcntl.LOCK_UN)
+        f.close()
+
+        f = open(path + 'result/' + fileSuffix, 'a')
+        fcntl.flock(f, fcntl.LOCK_EX)
+        f.write(prefix + attacked_bin + '\n')
+        fcntl.flock(f, fcntl.LOCK_UN)
+        f.close()
