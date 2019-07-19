8bb32d2e718fd4a5cef7405a85a4e017ebc577b8
Pavel Eis <xeispa00@stud.fit.vutbr.cz>
2019-07-19 14:38:50 +0200

    install: Updates Mongodb version from 3.6 to 4.0
    It looks like rewriting 3.6 to 4.0 was really everything, that had
    to be done, based on documentation.

--- install/install_basic_dependencies.sh
+++ install/install_basic_dependencies.sh
@@ -31,13 +31,13 @@ python3 setup.py install
 echo "** Installing MongoDB **"
 
 # Add repository
-echo '[mongodb-org-3.6]
+echo '[mongodb-org-4.0]
 name=MongoDB Repository
-baseurl=https://repo.mongodb.org/yum/redhat/$releasever/mongodb-org/3.6/x86_64/
+baseurl=https://repo.mongodb.org/yum/redhat/$releasever/mongodb-org/4.0/x86_64/
 gpgcheck=1
 enabled=1
-gpgkey=https://www.mongodb.org/static/pgp/server-3.6.asc
-' > /etc/yum.repos.d/mongodb-org-3.6.repo
+gpgkey=https://www.mongodb.org/static/pgp/server-4.0.asc
+' > /etc/yum.repos.d/mongodb-org-4.0.repo
 
 yum install -y -q mongodb-org
 
