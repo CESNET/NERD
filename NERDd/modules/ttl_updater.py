1041eabb86ece081bc2aaa28a789deb2c8bda689
Pavel Eis <xeispa00@stud.fit.vutbr.cz>
2020-02-05 13:58:09 +0100

    ttl_updater: fixed timedelta calculation and constants moved to configuration

--- NERDd/modules/ttl_updater.py
+++ NERDd/modules/ttl_updater.py
@@ -13,18 +13,19 @@ class TTLUpdater(NERDModule):
     """
     Module for updating TTL in highly active or long active IP address records.
     """
-    # minimum number of events, where IP address has to occur in last 7 days, to be marked as highly active
-    HIGHLY_ACTIVE_THRESHOLD = 1000
-    # TTL in days of highly active IP address
-    HIGHLY_ACTIVE_TTL = 14
-    # number of days, which IP address has to be in NERD, to be marked as long active
-    LONG_ACTIVE_THRESHOLD = 30
-    # TTL in days of long active IP address
-    LONG_ACTIVE_TTL = 28
-
     def __init__(self):
         self.log = logging.getLogger('TTLUpdater')
         self.log.setLevel("DEBUG")
+
+        # minimum number of events, where IP address has to occur in last 7 days, to be marked as highly active
+        self.highly_active_threshold = g.config['record_life_threshold']['highly_active']
+        # TTL in days of highly active IP address
+        self.highly_active_ttl = g.config['record_life_length']['highly_active']
+
+        # number of days, which IP address has to be in NERD, to be marked as long active
+        self.long_active_threshold = g.config['record_life_threshold']['long_active']
+        # TTL in days of long active IP address
+        self.long_active_ttl = g.config['record_life_length']['long_active']
 
         g.um.register_handler(
             self.check_ttl,    # function (or bound method) to call
@@ -36,16 +37,16 @@ class TTLUpdater(NERDModule):
 
     def check_high_activity(self, new_updates, rec):
         try:
-            if rec['events_meta']['total7'] > TTLUpdater.HIGHLY_ACTIVE_THRESHOLD:
-                record_ttl = datetime.utcnow() + timedelta(days=self.HIGHLY_ACTIVE_TTL)
+            if rec['events_meta']['total7'] > self.highly_active_threshold:
+                record_ttl = datetime.utcnow() + timedelta(days=self.highly_active_ttl)
                 new_updates.append(('set', '_ttl.highly_active', record_ttl))
         except KeyError:
             pass
 
     def check_long_activity(self, new_updates, rec):
-        ip_lifetime = (rec['ts_last_update'] - rec['ts_added']).days
-        if ip_lifetime > TTLUpdater.LONG_ACTIVE_THRESHOLD:
-            record_ttl = rec['ts_last_update'] + timedelta(days=self.LONG_ACTIVE_TTL)
+        ip_lifetime = (rec['last_activity'] - rec['ts_added']).days
+        if ip_lifetime > self.long_active_threshold:
+            record_ttl = rec['last_activity'] + timedelta(days=self.long_active_ttl)
             new_updates.append(('set', '_ttl.long_active', record_ttl))
 
     def check_ttl(self, ekey, rec, updates):
