50ff8814f731714a43aa2fa81c4a0d3506654769
Václav Bartoš <bartos@cesnet.cz>
2020-02-10 16:50:59 +0100

    web: several fixes in on-demand record creation

--- NERDweb/static/ip_poll.js
+++ NERDweb/static/ip_poll.js
@@ -4,36 +4,30 @@
 var pollInterval = setInterval(poll, 1000);
 var queryCounter = 0;
 
-function show_error(){
-    data_info = document.getElementsByClassName("notfound2");
-    data_info[0].innerHTML = "Error while trying to load the data!";
+function show_error(message){
+    var elem = document.getElementsByClassName("notfound-fetching")[0];
+    elem.className = "notfound-error";
+    elem.innerHTML = message;
 }
 
 function poll(){
+    console.log("Trying " + window.location.href + "/_is_prepared ...");
     fetch(window.location.href + "/_is_prepared")
         .then((response) => response.text())
-        .then(function(data){
-            if (data === "true"){
-                // when IP data is ready, stop polling
-                clearInterval(pollInterval);
-                if (queryCounter !== 0){
-                    // do not reload, if the data was ready before
-                    $( "#ip-entity-info" ).load(window.location.href + " #ip-entity-info" );
-                    queryCounter = 0;
-                }
+        .then(function(data) {
+            console.log("Response:", data);
+            if (data === "true") {
+                location.reload();
             }
-            else {
-                // stop polling after cca 30 seconds
-                if(queryCounter > 30){
-                    clearInterval(pollInterval);
-                    show_error();
-                }
+            else if (queryCounter > 30) { // stop polling after 30 seconds
+                clearInterval(pollInterval);
+                show_error("Timeout - backend is probably overloaded. Try again later.");
             }
             queryCounter++;
         })
         .catch(function(error){
             clearInterval(pollInterval);
-            console.log(error);
-            show_error();
+            console.log("Error when polling _is_prepared:", error);
+            show_error("Error while trying to load the data!");
         });
 }
