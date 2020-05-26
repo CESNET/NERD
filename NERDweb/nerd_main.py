b639687fd888efa51889dd94fc00a282e24df873
jakubjancicka <33009108+jakubjancicka@users.noreply.github.com>
2020-05-26 21:59:27 +0200

    web: Add Hilbert map (#23)
    Add page with visualization of IPv4 address space on NERD web. Visualization uses Hilbert curve to display IPv4 address space. Pixel on visualization represents a certain network and its value is sum of reputation scores of IP addresses in given network.
    
    It uses iframe for displaying visualization. It is probably a temporary solution but it is the quickest one due to the remaining time.

--- NERDweb/nerd_main.py
+++ NERDweb/nerd_main.py
@@ -1242,6 +1242,10 @@ def iplist():
         return Response('ERROR: Database connection error', 503, mimetype='text/plain')
 
 
+# ******************** Map ********************
+@app.route('/map/')
+def map_index():
+    return render_template("map.html", **locals())
 
 # ******************** Static/precomputed data ********************
 
