c5016fbfe7685941793355a20b1038677334da15
Maksim Naumenko <karyfeever@gmail.com>
2021-09-09 00:11:51 +0200

    changed the version of MongoDB from 4.0 to 5.0

--- install/install_basic_dependencies.sh
+++ install/install_basic_dependencies.sh
@@ -57,13 +57,13 @@ pip3 install -q -r $BASEDIR/pip_requirements_nerdweb.txt
 echob "** Installing MongoDB **"
 
 # Add repository
-echo '[mongodb-org-4.0]
+echo '[mongodb-org-5.0]
 name=MongoDB Repository
-baseurl=https://repo.mongodb.org/yum/redhat/$releasever/mongodb-org/4.0/x86_64/
+baseurl=https://repo.mongodb.org/yum/redhat/$releasever/mongodb-org/5.0/x86_64/
 gpgcheck=1
 enabled=1
-gpgkey=https://www.mongodb.org/static/pgp/server-4.0.asc
-' > /etc/yum.repos.d/mongodb-org-4.0.repo
+gpgkey=https://www.mongodb.org/static/pgp/server-5.0.asc
+' > /etc/yum.repos.d/mongodb-org-5.0.repo
 
 yum install -y -q mongodb-org
 
