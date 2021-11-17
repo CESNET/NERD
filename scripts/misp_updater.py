0f97bd0c6dbd40193ed23aef51b45fe8c63b50e1
Václav Bartoš <bartos@cesnet.cz>
2021-11-17 23:18:09 +0100

    misp_updater: Improved help, logs, simplified a piece of code
    - Improved some texts in help
    - Reordered some data loading tasks and rewritten corresponding log messages so the whole process is easier to understand from log.
    - Simplified a condition in process_ip() in order to not duplicate code.
    
    Overall functionality shouldn't be affected

--- scripts/misp_updater.py
+++ scripts/misp_updater.py
@@ -9,11 +9,10 @@ import logging
 from datetime import datetime, timedelta
 import argparse
 import os
-import re
 import sys
 from ipaddress import IPv4Address, AddressValueError
 
-from pymisp import ExpandedPyMISP, MISPAttribute
+from pymisp import ExpandedPyMISP
 
 # Add to path the "one directory above the current file location" to find modules from "common"
 sys.path.insert(0, os.path.abspath(os.path.join(os.path.dirname(os.path.abspath(__file__)), '..')))
@@ -28,12 +27,12 @@ DEFAULT_MONGO_DBNAME = 'nerd'
 
 # parse arguments
 parser = argparse.ArgumentParser(
-    prog="misp_receiver.py",
-    description="NERD standalone, which will synchronize NERD with data from MISP instance."
+    prog="misp_updater.py",
+    description="NERD standalone script to synchronize NERD with data from MISP instance."
 )
 parser.add_argument("-v", dest="verbose", action="store_true",
                     help="Verbose mode")
-parser.add_argument('-c', '--config', metavar='FILENAME', default='/etc/nerd/nerdd.yml',
+parser.add_argument('-c', '--config', metavar='CONFIG_FILE', default='/etc/nerd/nerdd.yml',
                     help='Path to configuration file (default: /etc/nerd/nerdd.yml)')
 parser.add_argument("--cert", metavar='CA_FILE',
                     help="Use this server certificate (or CA bundle) to check the certificate of MISP instance, useful when the server uses self-signed cert.")
@@ -41,7 +40,7 @@ parser.add_argument("--insecure", action="store_true",
                     help="Don't check the server certificate of MISP instance.")
 parser.add_argument("--since", action="store",
                     help="Specifies date from which should misp_updater query IP addresses from MISP. "
-                    "Expected format: YYYY-MM-DD. Default is taken from MISP ip lifetime stored in /etc/nerd.yml "
+                    "Expected format: YYYY-MM-DD. Default is taken from MISP ip lifetime stored in /etc/nerd/nerd.yml "
                     "(180 days by default).")
 parser.add_argument("--to", action="store",
                     help="Specifies date to which should misp_updater query IP addresses from MISP. "
@@ -378,32 +377,26 @@ def process_ip(ip_addr, ip_info):
 
     if events:
         live_till = youngest_date + timedelta(days=inactive_ip_lifetime)
-        if db_entity is not None:
-            # compare 'misp_events' attrib from NERD with events list, if not same --> insert, else do not insert
-            if db_entity.get('misp_events', {}) != events:
-                # construct new update request and send it
-                update_requests = [('set', 'misp_events', events), ('set', '_ttl.misp', live_till),
-                                   ('setmax', 'last_activity', youngest_date)]
-                tq.put_task('ip', ip_addr, update_requests)
-        else:
-            # ip address not even in NERD --> insert it
-            update_requests = [('set', 'misp_events', events), ('set', '_ttl.misp', live_till), ('setmax',
-                                                                'last_activity', youngest_date)]
+        # compare 'misp_events' attrib from NERD with events list, if not same --> insert, else do not insert
+        # if IP address not in NERD or it's 'misp_events' attrib is different from what is currently in MISP
+        # create/update the record with current data
+        if db_entity is None or db_entity.get('misp_events', {}) != events:
+            # construct new update request and send it
+            update_requests = [('set', 'misp_events', events), ('set', '_ttl.misp', live_till),
+                               ('setmax', 'last_activity', youngest_date)]
             tq.put_task('ip', ip_addr, update_requests)
 
 
 def main():
-    logger.info("Loading a list of all IPs in MISP (in given time interval) ...")
-    ip_all_selected_interval = get_all_ip_interval()
-    logger.info("Loaded {} IPs.".format(len(ip_all_selected_interval)))
-
+    logger.info("Step 1: Remove misp_events key of IP records in NERD for IPs that are not in MISP anymore ...")
     logger.info("Loading a list of all IPs in MISP (in last {} days) ...".format(inactive_ip_lifetime))
     ip_all = get_all_ips()
-    logger.info("Loaded {} IPs.".format(len(ip_all)))
+    logger.info("Found {} IPs.".format(len(ip_all)))
 
     # get all IPs with 'misp_events' attribute from NERD
     logger.info("Searching NERD for IP records with misp_events ...")
     db_ip_misp_events = db.find('ip', {'misp_events': {'$exists': True, '$not': {'$size': 0}}})
+    logger.info("Found {} IPs.".format(len(db_ip_misp_events)))
 
     # find all IPs that are in NERD but not in MISP anymore
     db_ip_misp_events = set(db_ip_misp_events) - set(ip_all)
@@ -414,9 +407,14 @@ def main():
             "{} NERD IPs don't have a (recent) entry in MISP anymore, removing corresponding misp_events keys...".format(
                 len(db_ip_misp_events)))
         for ip in db_ip_misp_events:
-            tq.put_task('ip', ip, [('remove', 'misp_events')])
+            tq.put_task('ip', ip, [('remove', 'misp_events'), ('remove', '_ttl.misp')])
+    else:
+        logger.info("No outdated records found, nothing to remove.")
 
-    logger.info("Checking and updating NERD records for all the IPs ...")
+    logger.info("Step 2: Create or update NERD records for all IPs present in MISP ...")
+    logger.info("Loading a list of all IPs in MISP (in given time interval) ...")
+    ip_all_selected_interval = get_all_ip_interval()
+    logger.info("Loaded {} IPs, checking and updating corresponding NERD records ...".format(len(ip_all_selected_interval)))
 
     for ip_addr, ip_info in ip_all_selected_interval.items():
         process_ip(ip_addr, ip_info)
