756f5cb639d8b5450eca23af33ed45c468996afe
Maksim Naumenko <karyfeever@gmail.com>
2022-03-03 21:28:23 +0200

    changed filtering of dshild records, added scheduler, added function to the cleaner to delete old dshield records, and changed web template for the dshield

--- NERDd/new_dshield.py
+++ NERDd/new_dshield.py
@@ -4,6 +4,8 @@ import os
 import logging
 from datetime import datetime
 import urllib.request
+from apscheduler.schedulers.background import BlockingScheduler
+import signal
 
 # Add to path the "one directory above the current file location" to find modules from "common"
 sys.path.insert(0, os.path.abspath(os.path.join(os.path.dirname(os.path.abspath(__file__)), '..')))
@@ -40,6 +42,13 @@ min_targets = config.get('dshield.min_targets')
 
 current_date = datetime.utcnow().date().strftime("%Y-%m-%d")
 
+scheduler = BlockingScheduler(timezone='UTC')
+
+# Signal handler to stop scheduler gracefully
+def sigint_handler(signum, frame):
+    logger.info("Signal {} received, going to stop".format({signal.SIGINT: "SIGINT", signal.SIGTERM: "SIGTERM"}.get(signum, signum)))
+    scheduler.shutdown(wait=True)
+
 def processFeed(feed_data):
     logger.info("Start processing the feed")
     ips = {}
@@ -48,8 +57,6 @@ def processFeed(feed_data):
         ip_addr = record_data[0].lstrip('0')
         reports = int(record_data[3])
         targets = int(record_data[4])
-        if ((reports < min_reports) or (targets < min_targets)):
-            continue
         if(ip_addr in ips):
             ips[ip_addr]["reports"] += reports
             ips[ip_addr]["targets"] += targets
@@ -59,6 +66,8 @@ def processFeed(feed_data):
     logger.info("Processed the feed")
     logger.info("Start creating tasks")
     for ip_addr in ips:
+        if (ips[ip_addr]["reports"] < min_reports) or (ips[ip_addr]["targets"] < min_targets):
+            continue
         tq_writer.put_task('ip', ip_addr, [
                                             ('array_upsert', 'dshield', {'date' : current_date},
                                             [('set', 'reports', ips[ip_addr]["reports"]),
@@ -66,8 +75,7 @@ def processFeed(feed_data):
                                           ], "dshield")
     logger.info("Tasks created")
 
-if __name__ == "__main__":
-
+def downloadFeed():
     logger.info("Downloading feed...")
     feed = urllib.request.urlopen(dshield_feed_url)
     if(feed.getcode() == 200):
@@ -77,4 +85,15 @@ if __name__ == "__main__":
         sys.exit(1)
     logger.info("Feed downloaded")
     processFeed(feed_data)
+
+if __name__ == "__main__":
+
+    downloadFeed()
+    # Start scheduler to get new feed every day, register signal handler
+    scheduler.add_job(downloadFeed, 'cron', hour='0')
+    signal.signal(signal.SIGINT, sigint_handler)
+    signal.signal(signal.SIGTERM, sigint_handler)
+    signal.signal(signal.SIGABRT, sigint_handler)
+    scheduler.start()
+    logger.info("Stopped")
 
