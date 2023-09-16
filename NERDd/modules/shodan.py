dab33d51d81324219720214b03c90531d4bfa6d6
Václav Bartoš <bartos@cesnet.cz>
2023-09-16 23:18:45 +0200

    shodan: multiple improvements, request rate monitoring
    - registered also on 'every1w' event (as an addition to '!NEW')
    - remove 'shodan' attribute if there were some data previously but are not anymore
    - some API replies (404, 429) handled more specifically
    - improved comments and log messages
    - logging rate of InternetDB API requests (by response type) using EventCountLogger (ECL)
    - ECL config updated
    - Munin plugin added for tracking the rate of InternetDB API requests

--- NERDd/modules/shodan.py
+++ NERDd/modules/shodan.py
@@ -1,5 +1,5 @@
 """
-NERD module getting Shodan information about an IP address (ports, tags, CPEs).
+NERD module getting Shodan information about an IP address from InternetDB API (ports, tags, CPEs).
 """
 import logging
 
@@ -12,39 +12,45 @@ class Shodan(NERDModule):
     """
     Shodan InternetDB module.
     
-    Retrieves information about a host from the Shodan Internet database. 
-    It returns the host's ports, tags, and CPEs if available, or None if the host is not found.
-    Stores the following attributes:
-      ports   # open ports
-      tags    # tags
-      cpes    # common platform enumerations
-    
-    Event flow specification:
+    Retrieves information about a host from the Shodan's Internet database.
+
+    The InternetDB API provides host's ports, tags, and CPEs if available. The data are updated once a week,
+    so we ask for each new IP address and then every one week.
+    The module stores the following attributes:
+      shodan.ports   # open ports
+      shodan.tags    # tags
+      shodan.cpes    # common platform enumerations
     """
     
     def __init__(self):
         self.log = logging.getLogger("Shodan")
-        self.log.info("Loaded")
+        self.log.setLevel("DEBUG")
+        self.log.debug("Module loaded")
+
+        # Event logging using EventCountLogger
+        # (if "shodan" group is not configured in EventCountLogger config, DummyEventGroup is returned, which does nothing)
+        # Log result of every request to InternetDB API (add_or_update_data, no_data, remove_old_data, rate_limit, unexpected_reply)
+        self.elog = g.ecl.get_group("shodan", True)
+
         g.um.register_handler(
             self.shodan,
             'ip',
-            ('!NEW',),
+            ('!NEW', '!every1w'),
             ('shodan.ports','shodan.tags','shodan.cpes')
         )
-    
+
     def shodan(self, ekey, rec, updates):
         """
-        Retrieves information about a host from the Shodan Internet database. 
+        Retrieves information about a host from the Shodan's Internet database.
         It returns the host's ports, tags, and CPEs if available, or None if the host is not found.
         
         Arguments:
         ekey -- two-tuple of entity type and key, e.g. ('ip', '192.0.2.42')
         rec -- record currently assigned to the key
-        updates -- list of all attributes whose update triggerd this call and  
+        updates -- list of all attributes whose update triggered this call and
           their new values (or events and their parameters) as a list of 
           2-tuples: [(attr, val), (!event, param), ...]
 
-        
         Returns:
         List of update requests.
         """
@@ -52,16 +58,36 @@ class Shodan(NERDModule):
         if etype != 'ip':
             return None   
      
-        host = requests.get(f"https://internetdb.shodan.io/{key}")
-        if host.status_code != 200:
+        reply = requests.get(f"https://internetdb.shodan.io/{key}")
+        if reply.status_code == 404:
+            # Shodan does not have any data about this IP address
+            if 'shodan' in rec:
+                # We already had some Shodan data in the record -> remove them
+                self.elog.log('remove_old_data')
+                self.log.debug(f"Shodan info for {key} not available anymore - removing 'shodan' attribute")
+                return [('remove', 'shodan')]
+            self.elog.log('no_data')
+            self.log.debug(f"Shodan info for {key} not available")
             return None
-        
-        host = host.json()
-        ports = host.get('ports', None)
-        tags = host.get('tags', None)
-        cpes = host.get('cpes', None)
-        
-        self.log.debug(f"Added Shodan info for {key}")
+        elif reply.status_code == 429:
+            # Rate limit exceeded
+            self.elog.log('rate_limit')
+            self.log.warning(f"InternetDB rate-limit exceeded")
+
+            return None
+        elif reply.status_code != 200:
+            self.elog.log('unexpected_reply')
+            # Log warning message - status code and at most 500 chars of content (presumably error message)
+            self.log.warning(f"Unexpected reply from InternetDB: ({reply.status_code}) {reply.text[:500]}")
+            return None
+
+        reply = reply.json()
+        ports = reply.get('ports', None)
+        tags = reply.get('tags', None)
+        cpes = reply.get('cpes', None)
+        self.elog.log('add_or_update_data')
+        self.log.debug(f"Shodan info for {key} available: {ports}, {tags}, {cpes}")
+
         return [
             ('set', 'shodan.ports', ports),
             ('set', 'shodan.tags', tags),
